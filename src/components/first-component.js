13b2a01c2e9b70d7f3d98e7639e93dd09cc14332
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-21 18:58:00 -0700

    remove lodash, update quickstart

--- src/components/first-component.js
+++ src/components/first-component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
+// This is a functional component rather than a class component
 const FirstComponent = () => {
-  // above arg is same as arg props plus const video = props.video;
 
   return (
     <h1>Hello from the FirstComponent</h1>
